4206a5f50a6d3f6ffcd6359083dfd6dd6bc6634b
Nika <Dayanova27@gmail.com>
2025-01-18 11:20:28 +0500

    test commit

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    eslintConfigPrettier,
+    eslintPluginPrettier,
+    {
+        files: ['src/**/*.js'],
+        ignores: ['**/*.config.js', 'dist'],
+    },
+    {
+        rules: {
+            'no-unused-vars': 'error',
+        },
+    },
+];
