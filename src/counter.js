a32c45a3824ca40676807ae03debb68cebf6a3e4
Nika <Dayanova27@gmail.com>
2025-01-18 11:22:56 +0500

    test commit

--- src/counter.js
+++ src/counter.js
@@ -5,6 +5,6 @@ export function setupCounter(element) {
         element.innerHTML = `count is ${counter}`;
     };
     element.addEventListener('click', () => setCounter(counter + 1));
-    setCounter(0);
+      setCounter(0)
 }
 const jdvkjvdvj = 1324343;
