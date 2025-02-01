11eb9b3c86d5c483094f117a4e87b913c56500bf
Nika <Dayanova27@gmail.com>
2025-02-01 11:47:09 +0500

    feat: add function night mode'

--- src/header/header-params.js
+++ src/header/header-params.js
@@ -9,6 +9,8 @@ const headerParams = {
         'max-w-7xl',
         'mx-auto',
         'w-[96%]',
+        'flex',
+        'justify-between',
     ],
     attributes: {},
 };
@@ -16,8 +18,37 @@ const headerParams = {
 const titleParams = {
     tagName: 'h1',
     text: 'To-Do',
-    classList: ['text-cyan-600', 'text-3xl', 'font-medium'],
+    classList: [
+        'text-cyan-600',
+        'text-3xl',
+        'font-medium',
+        'dark:text-red-600',
+    ],
     attributes: {},
 };
 
-export { headerParams, titleParams };
+const btnNightModeParams = {
+    tagName: 'button',
+    text: '',
+    classList: [
+        'h-10',
+        'w-10',
+        'bg-cyan-600',
+        'rounded-full',
+        'flex',
+        'justify-center',
+        'items-center',
+    ],
+    attributes: {},
+};
+
+const imgParams = {
+    tagName: 'img',
+    text: '',
+    classList: ['h-8', 'w-8'],
+    attributes: {
+        src: '/sun-icon.svg',
+    },
+};
+
+export { headerParams, titleParams, btnNightModeParams, imgParams };
--- src/header/header.js
+++ src/header/header.js
@@ -1,12 +1,30 @@
 import creator from '../main-helpers/creator';
-import { headerParams, titleParams } from './header-params';
+import {
+    btnNightModeParams,
+    headerParams,
+    imgParams,
+    titleParams,
+} from './header-params';
 
 const headerCreator = () => {
     const headerElement = creator(headerParams);
     const titleElement = creator(titleParams);
 
+    const btnNightModeElement = creator(btnNightModeParams);
+    const imgElement = creator(imgParams);
+    btnNightModeElement.append(imgElement);
+
+    btnNightModeElement.addEventListener('click', nightMode);
+
     headerElement.append(titleElement);
+    headerElement.append(btnNightModeElement);
+
     return headerElement;
+};
+
+const nightMode = () => {
+    const appContainer = document.querySelector('html');
+    appContainer.classList.toggle('dark');
 };
 
 export { headerCreator };
