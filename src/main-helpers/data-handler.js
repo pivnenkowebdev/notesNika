d67dfc3ed3fe01a3edf1cf849e2cf63082622670
Nika <Dayanova27@gmail.com>
2025-02-17 10:49:45 +0500

    feat: add function init data

--- src/main-helpers/data-handler.js
+++ src/main-helpers/data-handler.js
@@ -1,18 +1,35 @@
-// const key = "notes";
+const keyLocal = 'notes';
 
-// const dataToJSON = (data) => {
-//     const dataString = JSON.stringify(data);
-//     return dataString;
-// }
+const dataToJSON = (data) => {
+    const dataString = JSON.stringify(data);
+    return dataString;
+};
 
-// const dataToLocalStorage = (dataString) => {
-//     localStorage.setItem(key, dataString);
-// }
+const dataToLocalStorage = (key, dataString) => {
+    localStorage.setItem(key, dataString);
+};
 
-// const dataFromLocalStorage = (key) => {
-//     return localStorage.getItem(key);
-// }
+const dataFromLocalStorage = (key) => {
+    return localStorage.getItem(key);
+};
 
-// const dataParse = (dataString) => {
-//     return JSON.parse(dataString);
-// }
+const dataParse = (dataString) => {
+    return JSON.parse(dataString);
+};
+
+const initData = () => {
+    const isData = dataFromLocalStorage(keyLocal);
+    if (isData) {
+        return dataParse(isData);
+    } else {
+        const templateData = {
+            regular: [],
+            favorite: [],
+        };
+
+        dataToLocalStorage(keyLocal, dataToJSON(templateData));
+    }
+};
+
+const allNotes = initData();
+console.log(allNotes);
