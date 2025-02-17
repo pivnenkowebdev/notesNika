63a466eff6208662f021463a2b08af2592965258
Nika <Dayanova27@gmail.com>
2025-02-17 10:23:56 +0500

    feat: make functions for work with local storage

---
+++ src/main-helpers/data-handler.js
@@ -0,0 +1,18 @@
+// const key = "notes";
+
+// const dataToJSON = (data) => {
+//     const dataString = JSON.stringify(data);
+//     return dataString;
+// }
+
+// const dataToLocalStorage = (dataString) => {
+//     localStorage.setItem(key, dataString);
+// }
+
+// const dataFromLocalStorage = (key) => {
+//     return localStorage.getItem(key);
+// }
+
+// const dataParse = (dataString) => {
+//     return JSON.parse(dataString);
+// }
